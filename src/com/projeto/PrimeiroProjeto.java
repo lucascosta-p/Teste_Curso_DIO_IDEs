1dbfba8a611e118571f9669f1a585bbfa1efd47a
lucascosta-p <lucaspaulo.silvacosta10@gmail.com>
2021-12-31 15:06:38 -0300

    first commit

---
+++ src/com/projeto/PrimeiroProjeto.java
@@ -0,0 +1,16 @@
+package com.projeto;
+
+import com.projeto.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		// System.out.println("Hello World");
+
+		Gato gato = new Gato("Astolfino", "Preto", 102);
+		
+		System.out.println(gato);
+
+	}
+
+}
